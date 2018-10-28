a3b98ae8dfb5f00cf649155cc947d3724a865095
dlwaysi <dlwaysi@miltronicsinc.com>
2018-10-27 20:50:26 -0400

    Added Project Header Description

--- src/main.c
+++ src/main.c
@@ -1,3 +1,37 @@
+
+//***********************************************************************
+//*																		*
+//* Company:	Miltronics, Inc.									 	*
+//*				115 Industrial East										*
+//*				Clifton, NJ  07012										*
+//*																		*
+//*				Phone: (973) 471-7700									*
+//*																		*
+//* Engineer:	Daniel L. & Jesus E.									*
+//* 																	*
+//* Create Date:    12:00:00 10/27/2018	 								*
+//* Module Name:    main.c												*
+//* Project Name:   Custome Contact Management Database					*
+//* Target Devices: 32/64 Bit x86 Architectures							*
+//* Tool versions:  Code::Blocks/Sublime 3/GCC GDB/						*
+//*																		*
+//* Dependencies: 														*
+//*																		*
+//* Revision:		0.001												*
+//* Additional Comments:  Set your tab stops to 4.						*
+//*																		*
+//* (C) Miltronics, Inc. (2018)											*
+//* Miltronics, Inc. owns the sole copyright to this					*
+//* software. Under international copyright laws you (1) may not make	*
+//* a copy of this software except for the purposes of maintaining a	*
+//* single archive copy, (2) may not derive works herefrom, (3) may not	*
+//* distribute this work to others. These rights are provided for		*
+//* information clarification, other restrictions of rights may apply	*
+//* as well.															*
+//*																		*
+//***********************************************************************
+
+
 #include <stdio.h>
 #include <stddef.h>
 
